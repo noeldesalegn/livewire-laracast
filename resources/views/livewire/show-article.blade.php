f0530b2ec711ad586c4b1f2361f896cef4c41048
noeldesalegn <desalegnbekele2121@gmail.com>
2025-02-12 20:45:46 +0300

    7'th ep of livewire Using Full Page Components part one

---
+++ resources/views/livewire/show-article.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="mt-2">
+        <a href="/search" class="text-white font-medium rounded-md p-4 bg-indigo-600">Back</a>
+    </div>
+    <div class="mt-4">
+        <div class="pt-2">
+            <h1 class="text-2xl font-bold">{{ $article->title }}</h1>
+            <p class="text-gray-500">{{ $article->content }}</p>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Livewire\Search;
+use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/search', Search::class);
+Route::get('/articles/{id}', ShowArticle::class);
 
 Route::middleware([
     'auth:sanctum',
