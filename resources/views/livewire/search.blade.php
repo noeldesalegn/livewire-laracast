e22ffb9d72a9b557b3104b4c5f8fc273f392b7e5
noeldesalegn <desalegnbekele2121@gmail.com>
2025-02-18 17:13:06 +0300

    9'th ep of live-wire Using Events part one

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -3,17 +3,12 @@
         <div class="mt-2">
             <input
                     type="text"
-                    class="p-4 w-9/12 border rounded-md text-white bg-gray-700  "
+                    class="p-4 w-full border rounded-md text-white bg-gray-700  "
                     placeholder="Enter something to search"
                     wire:model.live.debounce.1000ms ="searchText"
                     placeholder="{{ $placeholder }}"
             >
-            <button class="text-white font-medium rounded-md p-4 disabled:opacity-30 bg-indigo-600"
-                    wire:click.prevent="clear()"
-                    {{ empty($searchText) ? 'disabled' : '' }}
-            >
-                Clear
-            </button>
+
         </div>
     </form>
     <livewire:search-results :results="$results" :show="!empty($searchText)" />
